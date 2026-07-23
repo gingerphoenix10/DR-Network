c5f21f563f54553b1028b43e4b902fa29e89466a
Phoenix <phoenix@gingerphoenix10.com>
2026-07-23 19:22:42 +0100

    Create auto-build script

---
+++ build.py
@@ -0,0 +1,106 @@
+# Builds the modded data and xdelta files with the UndertaleModTool
+# project applied for all base data files provided in BuildSources.json
+
+# This script required UndertaleModCli.exe (CLI version of UndertaleModTool)
+# located in either the project folder, or in your PATH.
+#
+# For Xdelta generation (on by default) you'll also need either
+# xdelta.exe or xdelta3.exe (same thing, just renamed) in one of the same paths
+
+import os
+import json
+import subprocess
+
+tempPath = dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(tempPath)
+
+buildPath = "build"
+buildXdelta = True
+
+# Create folders & check file validity
+if not os.path.isdir(buildPath):
+    os.mkdir(buildPath)
+
+if not os.path.isfile("BuildSources.json"):
+    print("BuildSources.json not found in project")
+    exit()
+
+data = {}
+with open("BuildSources.json", 'r') as sources:
+    try:
+        data = json.load(sources)
+    except json.decoder.JSONDecodeError:
+        print("Invalid Sources JSON file")
+        exit()
+
+if type(data) == list:
+    print("Invalid Sources JSON. Root should be dict, not list")
+    exit()
+
+# Begin building sources
+for key in data:
+    print(f"\nAttempting to build {key}.")
+
+    # JSON validation
+    if not "Data" in data[key]:
+        print(f"Source \"{key}\" doesn't contain a Data filepath. Skipping")
+        continue
+
+    if not "OutputFile" in data[key]:
+        print(f"Source \"{key}\" doesn't contain an Output file name. Skipping")
+        continue
+
+    if not os.path.isfile(data[key]["Data"]):
+        print(f"Unable to find source {key}'s data file at `{data[key]["Data"]}`. Skipping")
+        continue
+
+    sourceBuild = os.path.join(buildPath, key)
+    if not os.path.isdir(sourceBuild):
+        os.mkdir(sourceBuild)
+
+    # Start building project
+    outputFile = os.path.join(sourceBuild, data[key]["OutputFile"])
+    try:
+        subprocess.run(["UndertaleModCli.exe", "project", "build", "project.json", "-s", data[key]["Data"], "-d", outputFile]) 
+    except FileNotFoundError:
+        print("Unable to locate UndertaleModCli. Cancelling")
+        exit()
+
+    # Post-build (Delete json)
+
+    # A quirk of UndertaleModTool and by extension CLI is that it will recursively search for
+    # every json file anywhere in the project folder, whether it's related to the mod or not.
+    # It also *creates* a json file when building a mod however, so we must remove it otherwise
+    # it'll cause errors when trying to build from then on
+    backupFile = os.path.join(sourceBuild, "mod-backup-manifest.json")
+    if os.path.isfile(backupFile):
+        os.remove(backupFile)
+
+    if not os.path.isfile(outputFile):
+        print(f"Something went wrong whilst building source {key}")
+        continue
+
+    if not buildXdelta:
+        print("Success!")
+        continue
+
+    # Post-build (xdelta)
+    print("Build successful. Generating Xdelta")
+    outputXdelta = os.path.join(sourceBuild, key+".xdelta")
+    command = ["xdelta.exe", "-e", "-s", data[key]["Data"], outputFile, outputXdelta]
+    try:
+        subprocess.run(command) 
+    except FileNotFoundError:
+        try:
+            command[0] = "xdelta3.exe"
+            subprocess.run(command) 
+        except FileNotFoundError:
+            print("Build Xdelta is enabled, but xdelta.exe / xdelta3.exe could not be located. Skipping xdeltas")
+            buildXdelta = False
+            continue
+        
+    if not os.path.isfile(outputXdelta):
+        print(command)
+        print(f"Something went wrong whilst generating xdelta for source {key}")
+        continue
+    print("Success!")
